0675957825b02565bcfff87f7a26e45c1dcf68e4
SydykovAzim07 <sydykov.azim010@gmail.com>
2025-09-22 22:21:21 +0600

    изменение counter

--- src/oop_basics/day06/Counter.java
+++ src/oop_basics/day06/Counter.java
@@ -14,15 +14,18 @@ public class Counter {
     public static int getCounter() {
         return counter;
     }
+    public void printInfo(int index) {
+        System.out.println("Объект " + index + ", ID: " + id);
+
+    }
 
     public static void main(String[] args) {
         Counter c = new Counter();
         Counter c2 = new Counter();
         Counter c3 = new Counter();
-        System.out.println("Объект: 1, " + c.getId());
-        System.out.println("Объект: 2, " + c2.getId());
-        System.out.println("Объект: 3, " + c3.getId());
-        System.out.println("Общее количество объектов: " + Counter.getCounter());
+        c.printInfo(1);
+        c2.printInfo(2);
+        c3.printInfo(3);
 
     }
 }
